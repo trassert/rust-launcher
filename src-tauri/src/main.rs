efb85b3778a16969fe913cceae3d67560de511a1
16steyy <@.com>
2026-03-22 15:14:12 +0300

    fix: fix ely

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,12 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-fn main() {
+fn load_dotenv_files() {
     let _ = dotenvy::dotenv();
+    let _ = dotenvy::from_filename("../.env");
+    let _ = dotenvy::from_filename("src-tauri/.env");
+}
+
+fn main() {
+    load_dotenv_files();
     mc16launcher_lib::run()
 }
